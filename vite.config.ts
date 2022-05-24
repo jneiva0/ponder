066705b6ffdb2661c6e4a50c9247b4a46e6b75bf
Joao Neiva <jneiva42@gmail.com>
2022-05-24 12:02:16 -0300

    added initial pwa setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,17 @@
 import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import { readFileSync } from 'fs'
+import { defineConfig, PluginOption } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command, mode }) => {
+export default defineConfig(({}) => {
   return {
-    plugins: [react()],
+    server: {
+      https: {
+        key: readFileSync('./.cert/localhost-key.pem'),
+        cert: readFileSync('./.cert/localhost.pem'),
+      },
+    },
+    plugins: [VitePWA({}), react()] as PluginOption[],
   }
 })
